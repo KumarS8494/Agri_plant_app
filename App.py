f21ae53b1bd7c702cc150b66f9bdb87b3dff9ce6
KumarS8494 <kumartankhush786@gmail.com>
2025-08-06 12:52:01 +0530

    Change in App file due to deployement issue4

--- App.py
+++ App.py
@@ -2,30 +2,7 @@ import streamlit as st
 from ui import render_mainpage
 from ui_about import render_about
 
-# --- Better Sidebar Button Styling ---
-st.sidebar.markdown("""
-    <style>
-    [data-testid="stSidebar"] {
-        padding: 1rem !important;
-    }
 
-    [data-testid="stSidebar"] > div:first-child {
-        display: flex;
-        flex-direction: column;
-    }
-
-    [data-testid="stSidebar"] button {
-        width: 100% !important;
-        padding: 0.8rem 1rem !important;
-        font-size: 16px !important;
-        font-weight: 600 !important;
-        border-radius: 10px !important;
-        margin-bottom: 12px !important;
-        display: block !important;
-        box-sizing: border-box !important;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
 # --- Sidebar UI ---
 st.sidebar.markdown("## 🌿 CropsDiagnosis")
@@ -34,9 +11,9 @@ st.sidebar.markdown("AI-powered Crops diagnosis")
 if "page" not in st.session_state:
     st.session_state.page = "Home"
 
-if st.sidebar.button("🏠      Home       "):
+if st.sidebar.button("🏠      Home       ", use_container_width=True):
     st.session_state.page = "Home"
-if st.sidebar.button("📘 About the Project"):
+if st.sidebar.button("📘 About the Project", use_container_width=True):
     st.session_state.page = "About"
 
 # --- Page Routing ---
